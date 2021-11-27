87edaa0c3324776a5a12fcfc1339798193ed4555
Alfijah <ajsarban@gmail.com>
2021-11-27 18:59:20 +0100

    4.4 a afgerond

---
+++ Javascript/4 Functions scopes/opdracht4.4.js
@@ -0,0 +1,28 @@
+//A
+
+const namen = ['Alfijah', 'Fatos', 'Michael', 'Zico'];
+
+// function groepsLeden() {
+//     const elkLid = document.getElementById('naam').value;
+//     if(elkLid === namen.includes()) {
+//         console.log(elkLid + " is je groepslid!");
+//     } else {
+//         console.log("Intruder alert!");
+//     }
+// }
+
+// This code is old code that has been refactored as seen above!
+function groepsLeden() {
+    const elkLid = document.getElementById('naam').value;
+    if(elkLid.toLowerCase() === namen[0].toLowerCase()) {
+        console.log(elkLid + " is je groepslid!");
+    } else if(elkLid.toLowerCase() === namen[1].toLowerCase()) {
+        console.log(elkLid + " is je groepslid!");
+    } else if(elkLid.toLowerCase() === namen[2].toLowerCase()) {
+        console.log(elkLid + " is je groepslid!");
+    } else if(elkLid.toLowerCase() === namen[3].toLowerCase()) {
+        console.log(elkLid + " is je groepslid!");
+    } else {
+        console.log("Intruder alert!");
+    }
+}
