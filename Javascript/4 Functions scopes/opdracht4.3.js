a1031b1b302ef25b0592e470efae671dcde5927b
Alfijah <ajsarban@gmail.com>
2021-11-27 16:48:03 +0100

    done

---
+++ Javascript/4 Functions scopes/opdracht4.3.js
@@ -0,0 +1,24 @@
+//A
+function randomNumber() {
+    const result = Math.random();
+    console.log(result);
+}
+
+
+//B
+function randomNumber2(number) {
+    const result2 = Math.floor(Math.random() * number);
+    console.log(result2);
+}
+
+
+//C
+function checkMin() {
+    const minNumber = Math.min(8, 6, 3);
+    console.log(minNumber);
+}
+
+function checkMax() {
+    const maxNumber = Math.max(9, 7, 5);
+    console.log(maxNumber);
+}
