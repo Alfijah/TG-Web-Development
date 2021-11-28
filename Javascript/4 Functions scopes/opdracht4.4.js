c4bb801cb03e26e8878415ea312a8014fceb1678
Alfijah <ajsarban@gmail.com>
2021-11-28 22:10:01 +0100

    base

--- Javascript/4 Functions scopes/opdracht4.4.js
+++ Javascript/4 Functions scopes/opdracht4.4.js
@@ -5,31 +5,53 @@ const namen = ['Alfijah', 'Fatos', 'Michael', 'Zico'];
 // function groepsLeden() {
 //     const elkLid = document.getElementById('naam').value;
 //     if(elkLid === namen.includes()) {
-//         console.log(elkLid + " is je groepslid!");
+//         console.log(elkLid + " is je groe222222pslid!");
 //     } else {
 //         console.log("Intruder alert!");
 //     }
 // }
 
 // This code is old code that has been refactored as seen above!
+// let printNaam2 = function groepsLeden() {
+//    const elkLid = document.getElementById('naam').value;
+//     if(elkLid.toLowerCase() === namen[0].toLowerCase()) {
+//         console.log(elkLid + " is je groepslid!");
+//     } else if(elkLid.toLowerCase() === namen[1].toLowerCase()) {
+//         console.log(elkLid + " is je groepslid!");
+//     } else if(elkLid.toLowerCase() === namen[2].toLowerCase()) {
+//         console.log(elkLid + " is je groepslid!");
+//     } else if(elkLid.toLowerCase() === namen[3].toLowerCase()) {
+//         console.log(elkLid + " is je groepslid!");
+//     } else {
+//         console.log("Intruder alert!");
+//     }
+// }
+
 function groepsLeden() {
-    const elkLid = document.getElementById('naam').value;
-    if(elkLid.toLowerCase() === namen[0].toLowerCase()) {
-        console.log(elkLid + " is je groepslid!");
-    } else if(elkLid.toLowerCase() === namen[1].toLowerCase()) {
-        console.log(elkLid + " is je groepslid!");
-    } else if(elkLid.toLowerCase() === namen[2].toLowerCase()) {
-        console.log(elkLid + " is je groepslid!");
-    } else if(elkLid.toLowerCase() === namen[3].toLowerCase()) {
-        console.log(elkLid + " is je groepslid!");
-    } else {
-        console.log("Intruder alert!");
+    let elkLid = document.getElementById('naam').value.toString();
+
+    let isIncluded = namen.includes(elkLid.toLowerCase());
+    console.log(isIncluded);
+    if(isIncluded){
+        return elkLid + " is je groepslid.";
     }
-}
+    else{
+        return 'Intruder alert!';
+    }
+
+//     (e
+//     return elkLid + " is je groepslid!";
+//  } else {
+//      return 'Intruder alert!';
+ }
+ 
 
 
 //B
+
 function printResultaat() {
-    const printNaam = groepsLeden();
-    document.getElementById('print').innerHTML = printNaam;
+    let test4 = groepsLeden();
+    // let test = document.getElementById('print').value = groepsLeden();
+    document.getElementById('print').innerHTML = test4;
+    console.log(test4);
 }
