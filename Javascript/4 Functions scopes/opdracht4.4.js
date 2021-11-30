5406f663eff8af37847c4544b5e72c87492b2f0b
Alfijah <ajsarban@gmail.com>
2021-11-30 15:02:07 +0100

    derde function aangepast

--- Javascript/4 Functions scopes/opdracht4.4.js
+++ Javascript/4 Functions scopes/opdracht4.4.js
@@ -1,50 +1,36 @@
 //A
 
 const namen = ['Alfijah', 'Fatos', 'Michael', 'Zico'];
+let elkLid;
 
-// function groepsLeden() {
-//     const elkLid = document.getElementById('naam').value;
-//     if(elkLid === namen[]) {
-//         console.log(elkLid + " is je groepslid!");
-//     } else {
-//         console.log("Intruder alert!");
-//     }
-// }
-
-// This code is old code that has been refactored as seen above!
    function groepsLeden() {
-   let elkLid = document.getElementById('naam').value;
+     elkLid = document.getElementById('naam').value;  
     if(elkLid.toLowerCase() === namen[0].toLowerCase()) {
-        console.log(elkLid + " is je groepslid!");
+        console.log(elkLid + " is je groepslid.");
     } else if(elkLid.toLowerCase() === namen[1].toLowerCase()) {
-        console.log(elkLid + " is je groepslid!");
+        console.log(elkLid + " is je groepslid.");
     } else if(elkLid.toLowerCase() === namen[2].toLowerCase()) {
-        console.log(elkLid + " is je groepslid!");
+        console.log(elkLid + " is je groepslid.");
     } else if(elkLid.toLowerCase() === namen[3].toLowerCase()) {
-        console.log(elkLid + " is je groepslid!");
+        console.log(elkLid + " is je groepslid.");
     } else {
         console.log("Intruder alert!");
     }
-}
-
-function groepsLeden2() {
-    let elkLid = document.getElementById('naam').value.toString();
-
-    let isIncluded = namen.includes(elkLid);
-    if(isIncluded){
-        return elkLid + " is je groepslid.";
-    }
-    else{
-        return 'Intruder alert!';
-    }
- }
  
-
+}
 
 //B
-function printResultaat() {
-    let test4 = groepsLeden2();
-    // let test = document.getElementById('print').value = groepsLeden();
-    document.getElementById('print').innerText = test4;
-    // console.log(test4);
-}
+function groepsLeden2() {
+    elkLid = document.getElementById('naam').value;
+    let isIncluded = namen.includes(elkLid);
+    if(isIncluded){
+        return elkLid + ' is je groepslid.';
+    
+    } else{
+        return 'Intruder alert!'
+    }
+ }
+
+ function printResultaat() {
+     document.getElementById('print').innerText = groepsLeden2();
+ }
