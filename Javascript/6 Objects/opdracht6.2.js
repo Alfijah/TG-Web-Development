79c6d992b8a028222605d4065db56fec01359014
Alfijah <ajsarban@gmail.com>
2021-12-04 18:00:51 +0100

    C afgerond

--- Javascript/6 Objects/opdracht6.2.js
+++ Javascript/6 Objects/opdracht6.2.js
@@ -1,30 +1,74 @@
-function Club(naam, type, aantal) {
-    this.naam = naam
-    this.type = type
+const Club = function(naam, type, aantal) {
+    this.naam = naam,
+    this.type = type,
     this.aantal = aantal
-    function Contactinformatie(adres, telefoonnummer, contactpersoon) {
-        this.adres = adres
-        this.telefoonnummer = telefoonnummer
-        this.contactpersoon = contactpersoon
-    }
 }
 
-let soortenClubs = [('Swing', 'Golf', 8), ('Dunk', 'Basketball', 9)]
+const Contactinformatie = function(adres, telefoonnummer, contactpersoon) {
+    this.adres = adres,
+    this.telefoonnummer = telefoonnummer,
+    this.contactpersoon = contactpersoon
+}
 
-let club1 = soortenClubs.find(naam => naam === 'Swing');
-console.log(club1);
+let clubObject = new Club();
+let informatieObject = new Contactinformatie();
+clubObject.adres = informatieObject.adres;
+clubObject.telefoonnummer = informatieObject.telefoonnummer;
+clubObject.contactpersoon = informatieObject.contactpersoon;
 
+let mergeClubData = {
+    ...clubObject,
+    ...informatieObject
+};
 
+mergeClubData = [
+    {
+        naam: 'Dunk',
+        type: 'Baksetbal',
+        aantal: 9,
+        adres: 'Dunkkade',
+        telefoonnummer: 012345,
+        contactpersoon: 'Dunker'
+    },
+    {
+        naam: 'Vlinderslag',
+        type: 'Zwemmen',
+        aantal: 8,
+        adres: 'Vlinderkade',
+        telefoonnummer: 23456,
+        contactpersoon: 'Vlinder'
+    },
+    {
+        naam: 'Zwaai',
+        type: 'Golf',
+        aantal: 5,
+        adres: 'Zwaaikade',
+        telefoonnummer: 34567,
+        contactpersoon: 'Zwaaier'
+    },
+    {
+        naam: 'Race',
+        type: 'Mountainbike',
+        aantal: 12,
+        adres: 'Racekade',
+        telefoonnummer: 45678,
+        contactpersoon: 'Racer'
+    },
+    {
+        naam: 'Flash',
+        type: 'Hardlopen',
+        aantal: 15,
+        adres: 'Loopkade',
+        telefoonnummer: 56789,
+        contactpersoon: 'Flasher'
+    }
+]
 
-// for (let club in soortenClubs) {
-//     console.log(soortenClubs[club]);
-// }
+console.log(mergeClubData);
 
-
-
-
-// console.log(clubInformatie.Club);
-
-// for(let club in clubInformatie) {
-//     console.log(`${clubInformatie[0][0]}`);
-// }
+for(let c in mergeClubData) {
+    console.log(`
+    naam: ${mergeClubData[c].naam},
+    telefoonnummer: ${mergeClubData[c].telefoonnummer},
+    contactpersoon: ${mergeClubData[c].contactpersoon}`);
+}
