4db0c12a79d456bbe1d9a0f37376b884293c2025
Alfijah <ajsarban@gmail.com>
2021-12-15 14:42:22 +0100

    base

---
+++ Challenges/Edge_case/edge.js
@@ -0,0 +1,32 @@
+let inputValue = document.querySelector('.input');
+
+switch (inputValue) {
+    case 1:
+        console.log('een');
+        break;
+    case 2:
+        console.log('twee');
+        break;
+    case 3:
+        console.log('drie');
+        break;
+    case 4:
+        console.log('vier');
+        break;
+    case 5:
+        console.log('vijf');
+        break;
+    case 6:
+        console.log('zes');
+        break;
+    case 7:
+        console.log('zeven');
+        break;
+    case 8:
+        console.log('acht');
+        break;
+    case 9:
+        console.log('negen');
+        break;
+}
+
