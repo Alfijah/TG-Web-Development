0f5eba890ca7bd9bf48d0d8226ebbeae8729929c
Alfijah <ajsarban@gmail.com>
2021-12-16 21:46:31 +0100

    base

---
+++ Quiz/quiz.js
@@ -0,0 +1,8 @@
+const quiz = document.querySelector('.quiz');
+const div = document.createElement('div');
+const startBtn = document.createTextNode('Start');
+
+quiz.appendChild(div);
+div.append(startBtn);
+
+console.log(div)
