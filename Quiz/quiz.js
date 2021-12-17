bd3b1b5256667a3cd05d2c62c7d9f9a6324d4a5e
Alfijah <ajsarban@gmail.com>
2021-12-17 17:17:25 +0100

    layout v0

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -1,10 +1,38 @@
 //DEFINING VARIABLES
-const quizContainer = document.querySelector('.quiz-container');
-const divElement = document.createElement('div');
-const startBtn = divElement.classList.add('startBtn');
+const generalContainer = document.querySelector('.generalContainer');
+const startBtn = document.createElement('div');
+const moveBtns = document.createElement('div');
+const previousBtn = document.createElement('div');
+const nextBtn = document.createElement('div');
+const quizContainer = document.createElement('div');
 
-quizContainer.appendChild(divElement).innerText = 'Start';
+//ASSIGN CLASSNAME TO VARIABLES
+startBtn.classList.add('startBtn');
+moveBtns.classList.add('moveBtns');
+previousBtn.classList.add('previousBtn');
+nextBtn.classList.add('nextBtn');
+quizContainer.classList.add('quizContainer');
+
+//APPEND CHILDELEMENT TO VARIABLES
+generalContainer.appendChild(startBtn).innerText = 'Start';
+generalContainer.append(moveBtns, quizContainer);
+moveBtns.appendChild(previousBtn).innerText = 'Vorige';
+moveBtns.appendChild(nextBtn).innerText = 'Volgende';
 
 
 
-console.log(div)
+
+
+//BUILD THE FORM
+// generalContainer.appendChild(divElement).innerText = 'Start';
+// generalContainer.appendChild(divElement).innerText = 'Math Problem';
+
+
+//MAKE THE STARTBUTTON
+
+
+
+//MAKE THE NEXT AND PREVIOUS BUTTON
+
+
+//FUNCTIONS
