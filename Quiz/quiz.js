277ea67c7ae91f00633bb0c57ad5f6c72e2d52cc
Alfijah <ajsarban@gmail.com>
2021-12-17 20:58:28 +0100

    working on the q&a functions

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -19,20 +19,33 @@ generalContainer.append(moveBtns, quizContainer);
 moveBtns.appendChild(previousBtn).innerText = 'Vorige';
 moveBtns.appendChild(nextBtn).innerText = 'Volgende';
 
+//FUNCTIONS
+//MAKE THE STARTBUTTON
+function startQuiz() {
+    generalContainer.removeChild(startBtn);
+    //next add the form page
+}
 
-
-
+//
 
 //BUILD THE FORM
-// generalContainer.appendChild(divElement).innerText = 'Start';
-// generalContainer.appendChild(divElement).innerText = 'Math Problem';
+function quizForm(data) {
+    const quizTitle = document.createElement('div');
+    quizTitle.classList.add('quizTitle');
+    quizContainer.appendChild(quizTitle).innerText = 'Quiz Time!';
 
+    for (i=1; i<7; i++) {
+    const currentPage = document.createElement('div');
+    currentPage.classList.add('currentPage');
+    quizContainer.appendChild(currentPage).innerText = i + '/6';
+    }
+}
 
-//MAKE THE STARTBUTTON
+quizForm();
 
-
-
-//MAKE THE NEXT AND PREVIOUS BUTTON
 
 
 //FUNCTIONS
+//MAKE THE NEXT AND PREVIOUS BUTTON
+//ACTIVATE BUTTONS
+startBtn.onclick = startQuiz;
