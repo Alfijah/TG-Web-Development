60ffe64cc14f06da4a1158e405ffe17ace04e97d
Alfijah <ajsarban@gmail.com>
2021-12-20 16:39:39 +0100

    working on startbutton

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -4,13 +4,17 @@ const startBtn = document.createElement('div');
 const moveBtns = document.createElement('div');
 const previousBtn = document.createElement('div');
 const nextBtn = document.createElement('div');
-const quizContainer = document.createElement('div');
-const pageNumber = document.createElement('div');
+// const quizContainer = document.createElement('div');
 
 //MAKE THE STARTBUTTON
 startBtn.classList.add('startBtn');
 generalContainer.appendChild(startBtn).innerText = 'Start!';
 startBtn.onclick = startQuiz;
+
+function startQuiz() {
+    generalContainer.removeChild(startBtn);
+    //next add the form page
+}
 
 //MAKE THE PREVIOUS AND NEXT BUTTON
 moveBtns.classList.add('moveBtns');
@@ -30,12 +34,12 @@ const question = [
 ];
 
 const answers = [
-    {eachPage: ['Hyper Text Markup Language', 'How To Make Lumpia', 'Hyper Text Management Laguage', 'Hyper Things Managing Language', 'Hyper Things Making Lumpia']},
-    {eachPage: ['Surfboard', 'Ironing Board', 'Motherboard', 'Fatherboard', 'Blackboard']},
-    {eachPage: ['Internet Technology', 'Information Technology', 'Irritating Tuxedos', "Impossible Torpedo's", 'Ivanka Trump']},
-    {eachPage: ['Pea-en-gee', 'Ping', 'Pung', 'Pang', 'Portable Graphics Format']},
-    {eachPage: ['Pokemon', 'Chilli', 'Anti-virus software', 'Browser', 'Marvel Heroes']},
-    {eachPage: ['Vodka', 'Water', 'Jealousy', 'Communism', 'Nested Dolls']}
+    { eachPage: ['Hyper Text Markup Language', 'How To Make Lumpia', 'Hyper Text Management Laguage', 'Hyper Things Managing Language', 'Hyper Things Making Lumpia'] },
+    { eachPage: ['Surfboard', 'Ironing Board', 'Motherboard', 'Fatherboard', 'Blackboard'] },
+    { eachPage: ['Internet Technology', 'Information Technology', 'Irritating Tuxedos', "Impossible Torpedo's", 'Ivanka Trump'] },
+    { eachPage: ['Pea-en-gee', 'Ping', 'Pung', 'Pang', 'Portable Graphics Format'] },
+    { eachPage: ['Pokemon', 'Chilli', 'Anti-virus software', 'Browser', 'Marvel Heroes'] },
+    { eachPage: ['Vodka', 'Water', 'Jealousy', 'Communism', 'Nested Dolls'] }
 ];
 
 //BUILD THE FORM
@@ -74,7 +78,7 @@ function getQuestion(page) {
 }
 
 function getAnswers(page) {
-    switch(page) {
+    switch (page) {
         case 1:
             return getAnswersSeperated(0);
         case 2:
@@ -91,63 +95,53 @@ function getAnswers(page) {
 }
 
 function getAnswersSeperated(index) {
-    allAnswers.append(answerNumber, answerText);
-
-    for ( let i = 1; i < 7; i++) {
-        answerNumber.innerText = i;
-
-        for( a = 0; a < answers[index].eachPage.length; a++) {
-            answers[index].eachPage[a];
-
-            // console.log(answers[index].eachPage[a]);
-        }
-        
+    for (let a = 0; a < answers[index].eachPage.length; a++) {
+        return answers[index].eachPage[a];
+        //  console.log(answers[index].eachPage[a]);
     }
-console.log(i) 
+    // answers[index].eachPage.forEach((answer) => answer); 
 }
 
-// for (let a = 0; a < answers.length; a++) {
-//     for (let b = 0; b < answers[a].length; b++) {
-//         console.log(answers[b]);
-//     }
-// }
-
-quizContainer.classList.add('quizContainer');
+// quizContainer.classList.add('quizContainer');
 generalContainer.appendChild(quizContainer);
 class PageContent {
     constructor(page) {
+        this.quizContainer = document.createElement('div');
         this.quizTitle = document.createElement('div');
         this.pageNumber = document.createElement('div');
         this.questionBox = document.createElement('div');
         this.allAnswers = document.createElement('div');
-        this.answerBox = document.createElement('div');
-        this.answerNumber = document.createElement('div');
-        this.answerText = document.createElement('div');
-
+        
+        this.quizContainer.classList.add('quizContainer');
         this.quizTitle.classList.add('quizTitle');
         this.pageNumber.classList.add('pageNumber');
         this.questionBox.classList.add('questionBox');
         this.allAnswers.classList.add('allAnswers');
-        this.answerBox.classList.add('answerBox');
-        this.answerNumber.classList.add('answerNumber');
-        this.answerText.classList.add('answerText');
 
         this.quizTitle.innerText = "It's Quiz Time!";
         this.pageNumber.innerText = getPageNumber(page);
         this.questionBox.innerText = getQuestion(page);
-        // this.answerText.innerText = getAnswers(page);
 
         quizContainer.append(this.quizTitle, this.pageNumber, this.questionBox, this.allAnswers);
-        this.allAnswers.appendChild(this.answerBox);
-        this.answerBox.append(this.answerNumber, this.answerText);
+        
+        for (let i = 1; i < 6; i++) {
+            this.answerNumber = document.createElement('div');
+            this.answerText = document.createElement('div');
+            this.answerBox = document.createElement('div');
+
+            this.answerBox.classList.add('answerBox');
+            this.answerNumber.classList.add('answerNumber');
+            this.answerText.classList.add('answerText');
+
+            this.answerNumber.innerText = i;
+            this.answerText.innerText = getAnswers(page);
+
+            this.allAnswers.appendChild(this.answerBox);
+            this.answerBox.append(this.answerNumber, this.answerText);
+
+        }
     }
 
 }
 
 new PageContent(1);
-
-//FUNCTIONS
-function startQuiz() {
-    generalContainer.removeChild(startBtn);
-    //next add the form page
-}
